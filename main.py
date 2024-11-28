62ba3dfe20fc3883aadca532cad1f383f7a4ecd7
Emmanuel-Mafabi-Israel <mafabi_emmanuel@outlook.com>
2024-11-28 11:31:33 +0300

    mafabi: project creation and initialization

---
+++ main.py
@@ -0,0 +1,24 @@
+# OBJECT ORIENTED PROGRAMMING - PYTHON, ABSTRACTION
+# CREATING CLASSES IN PYTHON, UNDERLYING CONCEPTS AND IDEAS
+
+class Car:
+    # Class suite
+    # Class constructor -> __init__(self, ...)
+    # sound:str = ""
+    def __init__(self, make:str, model:str, year:str):
+        # instance variables -> attributes, member variables
+        self.make  = make
+        self.model = model
+        self.year  = year
+
+    # class methods, member functions
+    def start_engine(self):
+        print(f"{self.make} - Engine has started...")
+
+    def display_car_info(self):
+        print(f"Car make: {self.make}, Car Model: {self.model}, Assembly Year: {self.year}")
+
+# Creating the instances for the class -> Instantiation
+civic = Car("Honda", "Civic", "2018") # -> Object for the Class CAR
+civic.start_engine()     # output -> the car of this make - Engine has started...
+civic.display_car_info() # output -> info attached to the car
