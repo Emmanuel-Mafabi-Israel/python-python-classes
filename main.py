87533bad41cde474803b3c36a9939f66975427ca
Emmanuel-Mafabi-Israel <mafabi_emmanuel@outlook.com>
2024-11-29 15:33:19 +0300

    mafabi: modified main.py

--- main.py
+++ main.py
@@ -22,3 +22,13 @@ class Car:
 civic = Car("Honda", "Civic", "2018") # -> Object for the Class CAR
 civic.start_engine()     # output -> the car of this make - Engine has started...
 civic.display_car_info() # output -> info attached to the car
+
+sample_list:list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+def capitalize(lst:list):
+    return (word.upper() for word in lst)
+
+capitalized_words:list = capitalize(sample_list)
+
+for word in capitalized_words:
+    print(word)
